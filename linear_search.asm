11e6e4b2ea40079ff33e94eb0cfcb0eda5a7ca95
Mohit Arora <32641304+mohitarora3@users.noreply.github.com>
2017-11-15 19:38:09 +0530

    Perform linear search on an array

---
+++ linear_search.asm
@@ -0,0 +1,54 @@
+;........................................................................
+
+data segment
+    
+    array db 1,2,3,4,5
+    ele db 2		;ele variable stores the element to be searched
+    FOUND db  'ELEMENT FOUND $'
+    NOT_FOUND db 'ELEMENT NOT FOUND $'
+    
+data ends
+
+;...........................................................................
+
+
+;............................................................................
+
+code segment
+    
+    assume ds:data,cs:code
+    
+    start:
+         mov ax,data
+         mov ds,ax
+          
+         xor ax,ax		;initializing ax register with value 0
+         mov cx,5
+         mov bl,2
+         mov si,-1
+			
+    
+    next: 
+        inc si       
+        mov al,array[si]
+        cmp al,ele
+        je present			;if search is successful
+        loop next
+   
+        mov dx,offset NOT_FOUND		;if element is not present in arrray
+            mov ah,9
+            int 21h 
+            jmp exit 
+            
+     present: mov dx,offset FOUND
+            mov ah,9
+            int 21h
+       
+            
+     exit:hlt  
+        
+code ends
+   end start        
+   
+;.............................................................
+          
